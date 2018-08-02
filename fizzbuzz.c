cbb50d12f25a5ba36f7a62c6129f4fa0cf60bbb4
Carina Cruz <lecruz@bu.edu>
2018-08-02 23:28:54 +0000

    first commit

---
+++ fizzbuzz.c
@@ -0,0 +1,18 @@
+Using system;
+
+name FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
